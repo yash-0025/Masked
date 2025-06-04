a81c838ed1808fbf81b2d3bf1f891fb61d0c1c97
yash-0025 <yashpatel200403@gmail.com>
2025-06-05 02:44:22 +0530

    #SERVER :: SOCKET IO CONNECTION COMPLETED

---
+++ server/index.js
@@ -0,0 +1,50 @@
+import express from 'express';
+import {createServer} from 'http';
+import { Server } from 'socket.io';
+import cors from 'cors';
+
+const app = express();
+const httpServer = createServer(app);
+
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"],
+        credentials: true
+    }
+});
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true
+}));
+
+app.get('/', (req,res) => {
+    res.send("Masked Backend is up and running")
+});
+
+io.on('connection', (socket) => {
+    console.log(`User Connected: ${socket.id}`);
+
+    socket.on('sendMesage', (message) => {
+        console.log('Message Received :', message);
+        io.emit('receiveMessage', message);
+    })
+
+    socket.on('newPost', (postData) => {
+        console.log('New Post Notification Received', postData);
+        io.emit('newPostBroadcast', postData);
+    });
+
+    socket.on('disconnect', () => {
+        console.log(`User Disconnected : ${socket.id}`);
+    })
+})
+
+const PORT = process.env.PORT || 3001;
+httpServer.listen(PORT, () => {
+    console.log(`Backed Server is listening on PORT :: ${PORT}`);
+    console.log(`Socket.io Server is running on PORT :: ${PORT}`);
+})
