3aa1fd17977064db5a97f9911b6a8c27f14e2ec0
yash-0025 <yashpatel200403@gmail.com>
2025-06-07 15:51:05 +0530

    # FRONTEND :: INSTALLED REQUIRED DEPENDENCIES

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    tailwindcss(),
+  ],
+    define: {
+    'process.env' :{}
+  },
+   server: {
+    watch: {
+      usePolling: true, // Needed for Docker/WSL2 or some file systems
+      interval: 1000,  // Polling interval (ms)
+    },
+    hmr: true,         // Enable Hot Module Replacement
+  },
 })
